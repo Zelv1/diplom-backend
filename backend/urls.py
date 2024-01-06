a669075553f4e7d80655d5c48970af0f3ded1ab3
Zelv1 <zelenkevichivan950@gmail.com>
2024-01-06 15:20:57 +0300

    make endpoints for view

--- backend/urls.py
+++ backend/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from diplom.views import *
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('order_model/', OrderListInfoView.as_view()),
+    path('vendor_model/', VendorInfoView.as_view()),
+    path('courier_model/', CourierInfoView.as_view()),
+    path('customuser_model/', CustomUserInfoView.as_view()),
 ]
--- diplom/views.py
+++ diplom/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import *
+from .serializers import *
 
-# Create your views here.
+
+class OrderListInfoView(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class CourierInfoView(generics.ListCreateAPIView):
+    queryset = Courier.objects.all()
+    serializer_class = CourierSerializer
+
+
+class VendorInfoView(generics.ListCreateAPIView):
+    queryset = Vendor.objects.all()
+    serializer_class = VendorSerializer
+
+
+class CustomUserInfoView(generics.ListCreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+
